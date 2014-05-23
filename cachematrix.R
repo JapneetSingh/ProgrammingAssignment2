8bac405beb3f896ec8ed5e3e9acfa58ead098a8c
JapneetSingh <japneet13@gmail.com>
2014-05-23 02:10:13 -0700

    123

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
 ## The purpose of this code is to develop a cache to store the inverse of a matrix, so that it does not have to be calculated repeatedly.
-##This process is especially useful in case where it may be required to compute the inverse repeatedly, like in a loop, where the values of the initial matrix don't change much
+##This process is especially useful in case where it may be required to compute the inverse repeatedly, like in a loop,
+##where the values of the initial matrix don't change much
 ## We use two functions to complete this process 
 ##1)makeCacheMatrix():- this function serves to create a matrix that will act as the cache matrix. 
-##It accepts a square matrix that is invertible as an input and gives a list consisting of four functions as the output The calculated inverse matrix is stored here after being calculated for the first time.
+##It accepts a square matrix that is invertible as an input and gives a list consisting of four functions as the output 
+##The calculated inverse matrix is stored here after being calculated for the first time.
 ##
-##2)cacheSolve():- this function is used to calculate the inverse of the matrix input as an argument into the makeCacheMatrix() and then store its output in the in the same if not already stored . 
+##2)cacheSolve():- this function is used to calculate the inverse of the matrix input as an argument into the makeCacheMatrix() and 
+##then store its output in the in the same if not already stored . 
 ##It accepts the list created in makeCache Matrix() as the argument and  gives out the inverse of the matrix as the output.
 
 
@@ -31,8 +34,10 @@ makeCacheMatrix <- function(mat = matrix()) {
 
 
 ## cacheSolve() uses the list created by makeCacheMatrix() as the input.
-##It starts by checking if any value is stored in the inverse using the getinv() function. If the inverese was calculated in a previous iteration and stored in the cache it will use that as the output.
-##If the value is not stored in cache, it will oobtain the matrix using get()function,then use solve() function to calculate the inverse and then store it in the cache matrix using the setinv() 
+##It starts by checking if any value is stored in the inverse using the getinv() function. 
+##If the inverese was calculated in a previous iteration and stored in the cache it will use that as the output.
+##If the value is not stored in cache, it will oobtain the matrix using get()function,then use solve() function 
+##to calculate the inverse and then store it in the cache matrix using the setinv() 
 
 
 cacheSolve <- function(x, ...) {
